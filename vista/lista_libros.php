92987a0f247b34fb159eb1de7ee129c37a198708
Sofia Guerra <sofiguerra27@hotmail.com>
2023-06-29 00:28:34 -0300

    diseno de tabla listar y validacion

--- vista/lista_libros.php
+++ vista/lista_libros.php
@@ -11,35 +11,73 @@ ini_set('display_errors', 1);
     <link rel="stylesheet" href="publico/estilos.css">
 </head>
 
+<style>
+    table {
+        border-collapse: collapse;
+        width: 100%;
+    }
+
+    th, td {
+        padding: 8px;
+        text-align: left;
+        border-bottom: 1px solid #ddd;
+    }
+
+    th {
+        background-color: #f2f2f2;
+    }
+
+    .no-libros {
+        margin-top: 20px;
+        font-style: italic;
+    }
+</style>
+
 <body>
     <div class="container">
         <h1>Sistema de Gestión de Biblioteca</h1>
-        <?php $mensaje =$_SESSION['mensaje']; ?>
-            <div class="mensaje">
-                <?php  if(isset($mensaje)) echo $mensaje; ?>
-            </div>
-            <?php  ?>
+        <?php $mensaje = $_SESSION['mensaje']; ?>
+        <div class="mensaje">
+            <?php if (isset($mensaje)) echo $mensaje; ?>
+        </div>
 
         <!-- Agregar Usuario Formulario -->
         <h2>Lista de Libros</h2>
-        <ul class="list-group">
-            <?php foreach ($listaLibros as $libro) { ?>
-                <li class="list-group-item">
-                    <?php echo "Titulo: ".$libro['titulo']; ?> -
-                    <?php echo "Autor: ". $libro['autor']; ?> -
-                    <?php echo "Id: ". $libro['Id']; ?>
-                </li>
-            <?php } ?>
-        </ul>
+        <?php if (empty($listaLibros)) { ?>
+            <p class="no-libros">No hay libros registrados.</p>
+        <?php } else { ?>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Título</th>
+                        <th>Autor</th>
+                        <th>ID</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($listaLibros as $libro) { ?>
+                        <tr>
+                            <td><?php echo $libro['titulo']; ?></td>
+                            <td><?php echo $libro['autor']; ?></td>
+                            <td><?php echo $libro['Id']; ?></td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        <?php } ?>
+
         <div class="enlaces">
             <div class="enlace">
-                <h3><a href=" <?php echo $_SERVER['PHP_SELF']."?accion=menu"; ?>">Menú</a></h3>
+                <h3><a href="<?php echo $_SERVER['PHP_SELF'] . "?accion=menu"; ?>">Menú</a></h3>
             </div>
             <div class="enlace">
-                <h3><a href=" <?php echo $_SERVER['PHP_SELF']."?accion=salir"; ?>">Salir</a></h3>
+                <h3><a href="<?php echo $_SERVER['PHP_SELF'] . "?accion=salir"; ?>">Salir</a></h3>
             </div>
         </div>
     </div>
 </body>
+
+
+
 
 </html>
